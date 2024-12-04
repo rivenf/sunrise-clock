a0676b24b3ab1433ec7235a749faff60c2a2cba0
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-12-04 16:05:19 +0000

    Create React app

---
+++ sunrise-clock/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Clock from "Clock.js";
+
+function App() {
+  return (
+    <div className="container">
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ sunrise-clock/src/Clock.js
@@ -0,0 +1,7 @@
+export default function Clock() {
+    return (
+        <>
+            <p>more stuff and things</p>
+        </>
+    )
+}
